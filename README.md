58ab21db51d26c8e1aced1fffd7e20473b298a07
Antonio Maffia <maffia@dyn-36-197.mobile.unibas.ch>
2020-10-17 16:20:30 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# Container recipes for TensorFlow
---
+++ build_docker.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# SW_FLD=${TRAVIS_COMMIT_MESSAGE#*"Build docker:"}
+# SW_FLD=${SW_FLD%%,*}
+# SW_NAME=`echo ${SW_FLD%%-*} | sed 's/ *$//g'`
+# DOCKER_SW_NAME=${SW_NAME,,}
+# SW_TAG=${SW_FLD#*-}
+
+SW_NAME=`echo ${TRAVIS_REPO_SLUG##*/} | sed 's/ *$//g'`
+DOCKER_SW_NAME=${SW_NAME,,}
+SW_TAG=${TRAVIS_COMMIT_MESSAGE#*"Build docker:"}
+SW_TAG=${SW_TAG%%,*}
+
+export IMAGE_NAME="${DOCKER_ORG}/${DOCKER_SW_NAME}:${SW_TAG}"
+docker build --tag ${IMAGE_NAME} ./${SW_TAG}
