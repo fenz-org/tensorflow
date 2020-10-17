e76acf151ecca2abf8d47e73d5ab6f0fc09d27da
Antonio Maffia <maffia@dyn-36-197.mobile.unibas.ch>
2020-10-17 16:46:27 +0200

    Build docker: 2.1.0_c_anaconda-Python-3.7, try 2

--- build_docker.sh
+++ build_docker.sh
@@ -8,8 +8,11 @@
 
 SW_NAME=`echo ${TRAVIS_REPO_SLUG##*/} | sed 's/ *$//g'`
 DOCKER_SW_NAME=${SW_NAME,,}
+echo ${TRAVIS_COMMIT_MESSAGE}
 SW_TAG=${TRAVIS_COMMIT_MESSAGE#*"Build docker:"}
+echo ${SW_TAG}
 SW_TAG=${SW_TAG%%,*}
+echo ${SW_TAG}
 
 export IMAGE_NAME="${DOCKER_ORG}/${DOCKER_SW_NAME}:${SW_TAG}"
 docker build\
