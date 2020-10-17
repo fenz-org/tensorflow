a53743a5862705b2cbc20dd30a38bcfb3c641829
Antonio Maffia <maffia@dyn-36-197.mobile.unibas.ch>
2020-10-17 16:33:42 +0200

    Build docker: 2.1.0_c_anaconda-Python-3.7, add new TensorFlow version 2.1.0_c_anaconda-Python-3.7

--- build_docker.sh
+++ build_docker.sh
@@ -12,4 +12,9 @@ SW_TAG=${TRAVIS_COMMIT_MESSAGE#*"Build docker:"}
 SW_TAG=${SW_TAG%%,*}
 
 export IMAGE_NAME="${DOCKER_ORG}/${DOCKER_SW_NAME}:${SW_TAG}"
-docker build --tag ${IMAGE_NAME} ./${SW_TAG}
+docker build\
+  --label "BUILT-BY=Travis"\
+  --label "BUILT-FROM=${TRAVIS_REPO_SLUG}"\
+  --label "BUILD-LOG=${TRAVIS_BUILD_WEB_URL}"\
+  --tag ${IMAGE_NAME}\
+  ./${SW_TAG}
